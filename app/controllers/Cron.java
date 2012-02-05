c8202250e3ec46252083ede475c94da0cc813197
malys <domino0028@gmail.com>
2012-02-05 14:11:27 +0100

    change message

--- app/controllers/Cron.java
+++ app/controllers/Cron.java
@@ -13,72 +13,65 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-
-
 import models.Contact;
 import models.Link;
 import play.Logger;
-import play.mvc.*;
+import play.mvc.Controller;
 
 public class Cron extends Controller {
-
-	
 
 	/**
 	 * Envoi de l'email
 	 */
-    public static void send() {
-    	
-    	List<Contact> list = Contact.all().fetch(); 
-    	Iterator<Contact> iter= list.iterator();
-    	while(iter.hasNext()){
-    		String body ="";
-    		Contact contact = iter.next();
-    		List<Link> listLink = Link.getByNotModified(contact);
-    		Iterator<Link> iterLink= listLink.iterator();
-        	while(iterLink.hasNext()){
-        		Link link = iterLink.next();
-        		Logger.debug("send " + contact.firstName + " " + link.url );
-        	//	body  = body +"\n"+ "<a href=\""+ link.url +  "\">" +  link.url  + "</a>";
-        		body  = body +"\n"+ link.url + "\n";
-        		link.isModified = true;
-        		link.save();
-        	}
-        	
-        	body  = body +"\nEnjoy this links.\n\n Regargs,\nLink Me First by Malys";
-        	
-        	if(body.length()>0){
-        		sendEmail(body,contact.emailAddress);
-        	}
-      	
-    	}
-    }
-    
-    /**
-     * Nettoyage
-     */
-    public static void clean() {
+	public static void send() {
 
-    		List<Link> listLink = Link.getModified();
-    		Iterator<Link> iterLink= listLink.iterator();
-        	while(iterLink.hasNext()){
-        		Link link = iterLink.next();
-        		link.delete();
-        	}
+		List<Contact> list = Contact.all().fetch();
+		Iterator<Contact> iter = list.iterator();
+		while (iter.hasNext()) {
+			String body = "";
+			Contact contact = iter.next();
+			List<Link> listLink = Link.getByNotModified(contact);
+			Iterator<Link> iterLink = listLink.iterator();
+			while (iterLink.hasNext()) {
+				Link link = iterLink.next();
+				Logger.debug("send " + contact.firstName + " " + link.url);
+				// body = body +"\n"+ "<a href=\""+ link.url + "\">" + link.url
+				// + "</a>";
+				body = body + "\n" + link.url + "\n";
+				link.isModified = true;
+				link.save();
+			}
 
-    }
-    
-    
-      
-  
-    
-    private static  void sendEmail(String body,String email){
-    	try {
-    		Properties props = new Properties();
+			body = body
+					+ "\nEnjoy this links.\n\nRegards,\n	Link Me First by Malys";
+
+			if (body.length() > 0) {
+				sendEmail(body, contact.emailAddress);
+			}
+
+		}
+	}
+
+	/**
+	 * Nettoyage
+	 */
+	public static void clean() {
+
+		List<Link> listLink = Link.getModified();
+		Iterator<Link> iterLink = listLink.iterator();
+		while (iterLink.hasNext()) {
+			Link link = iterLink.next();
+			link.delete();
+		}
+
+	}
+
+	private static void sendEmail(String body, String email) {
+		try {
+			Properties props = new Properties();
 			Session session = Session.getDefaultInstance(props, null);
 			Message msg = new MimeMessage(session);
-		    msg.setContent
-		         (body, "text/html; charset=ISO-8859-1");
+			msg.setContent(body, "text/html; charset=ISO-8859-1");
 
 			msg.setFrom(new InternetAddress("domino0028@gmail.com"));
 
@@ -87,19 +80,16 @@ public class Cron extends Controller {
 			msg.setSubject("Enlacitos");
 			msg.setText(body);
 			Transport.send(msg);
-			Logger.info("Envoi: "+body);
+			Logger.info("Envoi: " + body);
 
 		} catch (AddressException e) {
-			Logger.error(e.getMessage() );
+			Logger.error(e.getMessage());
 		} catch (MessagingException e) {
-			Logger.error(e.getMessage() );
+			Logger.error(e.getMessage());
 		} catch (UnsupportedEncodingException e) {
-			Logger.error(e.getMessage() );
+			Logger.error(e.getMessage());
 		}
-    	
-    }
-    
-    
-    
+
+	}
 
 }
