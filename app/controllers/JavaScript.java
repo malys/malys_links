094e206c5a2b18203c8e2e32ffeb9671b58fd2cc
malysus <domino0028@gmail.com>
2013-11-10 18:59:06 +0100

    add new feature

--- app/controllers/JavaScript.java
+++ app/controllers/JavaScript.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.List;
+
 import models.Contact;
 import models.Link;
 import play.Logger;
@@ -20,4 +24,22 @@ public class JavaScript extends Controller {
 		}
 	}
 
+	public static void insertPOST(String name, String url, String comment)
+			throws UnsupportedEncodingException {
+
+		Contact contact = Contact.getByFirstName(name);
+		if (contact != null) {
+			Link link = new Link(URLDecoder.decode(url, "UTF-8"), contact);
+			if (comment != null) {
+				link.comment = URLDecoder.decode(comment, "UTF-8") + "\n";
+			}
+			Logger.info("Add " + url.toString() + " for " + contact.firstName);
+			link.save();
+		}
+	}
+
+	public static void getModifiedUrls(String name) {
+		List<Link> listLink = Link.getByModified(Contact.getByFirstName(name));
+		renderJSON(listLink);
+	}
 }
--- app/models/Link.java
+++ app/models/Link.java
@@ -4,90 +4,89 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-import play.*;
-import play.db.jpa.*;
 import siena.Column;
 import siena.DateTime;
-import siena.Generator;
 import siena.Id;
 import siena.Index;
 import siena.Max;
-import siena.NotNull;
-import siena.Query;
 import siena.Table;
-import siena.core.Many;
-import siena.core.Owned;
 import siena.core.batch.Batch;
+
+import com.sun.istack.internal.NotNull;
 
 @Table("links")
 public class Link extends siena.Model {
-    @Id(Generator.AUTO_INCREMENT)
-    public Long id; 
+	@Id(siena.Generator.AUTO_INCREMENT)
+	public Long id;
 
-    @Column("url")
-    @Max(100) @NotNull
-    public String url;    
-    
-    @Column("modified")
-    @NotNull
-    @Index("modified")
-    public Boolean isModified; 
-    
-    
-    @Index("contact")
-    public Contact contact;
-    
-    @Column("created")
-    @NotNull @DateTime
-    public Date created;
- 
-    
-    public Link(){
-    	super();
-    }
-    
-    public Link(String url,Contact contact){
-    	this.url = url.replace("@","/");
-    	isModified = false;
-    	created = Calendar.getInstance().getTime();
-    	this.contact = contact;
-    	
-    }
-    
+	@Column("url")
+	@Max(100)
+	@NotNull
+	public String url;
 
-    
-    public String toString(){
-    	return "id: "+id+", url: "+url ;
-    }
-    
-    public static Query<Link> all() {
-    	return siena.Model.all(Link.class);
-    }
-    
-    public static Batch<Link> batch() {
-    	return siena.Model.batch(Link.class);
-    }
-    
-    public static Link getByUrl(String url){
-    	return all().filter("url", url).get();
-    }
-    
-    public static List<Link> getByContact(Contact contact){
-    	return all().filter("contact", contact).fetch();
-    }
-    
-    public static List<Link> getModified(){
-    	return all().filter("isModified", true).fetch();
-    }
-    
-    public static List<Link> getByNotModified(Contact contact){
-    	return all().filter("isModified", false).filter("contact", contact).fetch();
-    	
-    	
-    }
-    
+	@Column("modified")
+	@NotNull
+	@Index("modified")
+	public Boolean isModified;
+
+	@Index("contact")
+	public Contact contact;
+
+	@Column("comment")
+	public String comment;
+
+	@Column("created")
+	@NotNull
+	@DateTime
+	public Date created;
+
+	public Link() {
+		super();
+	}
+
+	public Link(String url, Contact contact) {
+		this.url = url.replace("@", "/");
+		isModified = false;
+		created = Calendar.getInstance().getTime();
+		this.contact = contact;
+
+	}
+
+	@Override
+	public String toString() {
+		return "id: " + id + ", url: " + url;
+	}
+
+	public static siena.Query<Link> all() {
+		return siena.Model.all(Link.class);
+	}
+
+	public static Batch<Link> batch() {
+		return siena.Model.batch(Link.class);
+	}
+
+	public static Link getByUrl(String url) {
+		return all().filter("url", url).get();
+	}
+
+	public static List<Link> getByContact(Contact contact) {
+		return all().filter("contact", contact).fetch();
+	}
+
+	public static List<Link> getModified() {
+		return all().filter("isModified", true).fetch();
+	}
+
+	public static List<Link> getByNotModified(Contact contact) {
+		return all().filter("isModified", false).filter("contact", contact)
+				.fetch();
+
+	}
+
+	public static List<Link> getByModified(Contact contact) {
+		return all().filter("isModified", true).filter("contact", contact)
+				.fetch();
+
+	}
 
 }
-
-
-
